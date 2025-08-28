bb8d3928045532d2c077fd95448f1ceb075d2572
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2025-08-28 23:03:23 +0530

    cookies setup in login & my profile

--- src/app.js
+++ src/app.js
@@ -2,152 +2,191 @@ const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const { validateSignupData } = require("./utils/validation");
-const bcrypt=require("bcrypt");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 
-const app = express()
+const app = express();
 app.use(express.json()); //middleware to parse JSON bodies
+app.use(cookieParser());
 
-app.post("/signup", async(req, res) => {
- 
+app.post("/signup", async (req, res) => {
   try {
-     //validation on data signup api
-  validateSignupData(req)
-  const {firstName, lastName, emailId, password}=req.body
-  
-  //encryption the password
-  const hashPassword = await bcrypt.hash(password,10)
-  console.log(hashPassword);
-  
-    const user= new User({
+    //validation on data signup api
+    validateSignupData(req);
+    const { firstName, lastName, emailId, password } = req.body;
+
+    //encryption the password
+    const hashPassword = await bcrypt.hash(password, 10);
+    console.log(hashPassword);
+
+    const user = new User({
       firstName,
       lastName,
       emailId,
-      password:hashPassword
+      password: hashPassword,
     });
     const savedUser = await user.save();
-    res.status(201).json({message: "User created successfully", user: savedUser});
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: savedUser });
   } catch (error) {
-    res.status(500).json({message: "Error creating user", error: error.message});//error handling
+    res
+      .status(500)
+      .json({ message: "Error creating user", error: error.message }); //error handling
   }
-})
+});
 //user login api
-app.post("/login", async(req,res)=>{
+app.post("/login", async (req, res) => {
   try {
-    const {emailId,password}=req.body
+    const { emailId, password } = req.body;
 
-   const user = await User.findOne({emailId:emailId})
-   if(!user){
-    throw new Error("Invalid Credentials");
-    
-   }
-    const isPasswordValid = await bcrypt.compare(password,user.password)
-    if(isPasswordValid){
-      res.status(200).json({message:"user login Successfully",user})
-    }else{
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
       throw new Error("Invalid Credentials");
-      
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      //create a jwt token
+      const token = await jwt.sign({ _id: user._id }, "Dev@Tinder123");
+      console.log(token);
+
+      res.cookie("token", token);
+
+      res.status(200).json({ message: "user login Successfully", user });
+    } else {
+      throw new Error("Invalid Credentials");
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching user", error: error.message }); //error handling
+  }
+});
+//get profile api
+app.get("/profile", async (req, res) => {
+  try {
+    const cookies = req.cookies;
+    const { token } = cookies;
+    if (!token) {
+      throw new Error("Invalid token");
+    }
+    //validate my token
+    const decodedMessage = await jwt.verify(token, "Dev@Tinder123");
+    const { _id } = decodedMessage;
+    console.log("loged in user is" + _id);
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("user does not exist");
     }
 
+    res.status(200).json(user);
   } catch (error) {
-    res.status(500).json({ message: "Error fetching user", error: error.message }); //error handling
-  
+    res.status(400).json({ message: "Error getting user data", error: error.message }); //error handling
   }
-})
+});
 //get user by emailId
 app.get("/user", async (req, res) => {
-  const  userEmail  = req.body.emailId; //extract emailId from request parameters
+  const userEmail = req.body.emailId; //extract emailId from request parameters
   try {
-    const users = await User.find({ emailId:userEmail }); //find user by emailId
+    const users = await User.find({ emailId: userEmail }); //find user by emailId
     if (!users) {
       return res.status(404).json({ message: "User not found" }); //if user not found, return 404
-    } 
+    }
     res.status(200).json(users); //return user data
   } catch (error) {
-    res.status(500).json({ message: "Error fetching user", error: error.message }); //error handling
+    res
+      .status(500)
+      .json({ message: "Error fetching user", error: error.message }); //error handling
   }
 });
 
 //feed api get/feed -- get vall the users from the database
-app.get("/feed", async(req,res)=>{
+app.get("/feed", async (req, res) => {
   try {
     const users = await User.find({}); //fetch all users from the database
     res.status(200).json(users); //return the list of users
   } catch (error) {
-    res.status(500).json({ message: "Error fetching users", error: error.message }); //errorhandling
+    res
+      .status(500)
+      .json({ message: "Error fetching users", error: error.message }); //errorhandling
   }
-})
+});
 //user find by id
-app.get("/user/:id",async(req,res)=>{
-  const userId=req.params.id;
+app.get("/user/:id", async (req, res) => {
+  const userId = req.params.id;
   try {
-    const users=await User.findById({_id:userId})
-    if(!users){
-      return res.status(404).json({message:"user not found"})
-
-    }else{
-      res.status(200).json(users)
+    const users = await User.findById({ _id: userId });
+    if (!users) {
+      return res.status(404).json({ message: "user not found" });
+    } else {
+      res.status(200).json(users);
     }
-    
   } catch (error) {
-    res.status(500).json({message:"error fetching user",error:error.message})
-    
+    res
+      .status(500)
+      .json({ message: "error fetching user", error: error.message });
   }
-})
+});
 //delete user by id
-app.delete("/user/:id",async(req,res)=>{
-  const userId= req.params.id;
-  try{
-    const users = await User.findByIdAndDelete({_id: userId})
-    if(!users){
-      res.status(404).json({message:"user not found"})
-    }else{
-      res.status(200).json({message:"user deleted successfully"})
+app.delete("/user/:id", async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const users = await User.findByIdAndDelete({ _id: userId });
+    if (!users) {
+      res.status(404).json({ message: "user not found" });
+    } else {
+      res.status(200).json({ message: "user deleted successfully" });
     }
-  }catch(error){
-    res.status(500).json({message:"error deleting user",error:error.message})
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "error deleting user", error: error.message });
   }
-})
+});
 
 //update user by id
-app.patch("/user/:id",async(req,res)=>{
-  const userId= req.params?.id;
-  const updateData=req.body;
+app.patch("/user/:id", async (req, res) => {
+  const userId = req.params?.id;
+  const updateData = req.body;
   try {
-    const ALLOWED_UPDATE=["gender","skills","age","photoUrl","about"]
-    const isAllowedUpdate=Object.keys(updateData).every((k)=>ALLOWED_UPDATE.includes(k))
-    if(!isAllowedUpdate){
+    const ALLOWED_UPDATE = ["gender", "skills", "age", "photoUrl", "about"];
+    const isAllowedUpdate = Object.keys(updateData).every((k) =>
+      ALLOWED_UPDATE.includes(k)
+    );
+    if (!isAllowedUpdate) {
       throw new Error("update not allowed");
-      
     }
     // if(updateData?.skills.length >10){
     //   throw new Error("skills can not be add more than 10")
     // }
-    const users= await User.findByIdAndUpdate({_id:userId},updateData,{new:true,runValidators:true})
-    if(!users){     
-      res.status(404).json({message:"user not found"})
-    }else{
-      res.status(200).json({message:"user updated successfully",user:users})
+    const users = await User.findByIdAndUpdate({ _id: userId }, updateData, {
+      new: true,
+      runValidators: true,
+    });
+    if (!users) {
+      res.status(404).json({ message: "user not found" });
+    } else {
+      res
+        .status(200)
+        .json({ message: "user updated successfully", user: users });
       console.log(users);
-      
     }
   } catch (error) {
-    res.status(500).json({message:"error updating user",error:error.message})
-    
+    res
+      .status(500)
+      .json({ message: "error updating user", error: error.message });
   }
-
-})
-
+});
 
 //db connection with server start
 connectDB()
-.then(()=>{
-  console.log("database connected");
-  app.listen(7777,()=>{
-  console.log("server running port 7777");
-  
-})
-}).catch((err)=>{
-  console.error("database not connected",err);
-  
-})
-
+  .then(() => {
+    console.log("database connected");
+    app.listen(7777, () => {
+      console.log("server running port 7777");
+    });
+  })
+  .catch((err) => {
+    console.error("database not connected", err);
+  });
