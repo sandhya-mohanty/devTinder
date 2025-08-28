56bc7b989f1ad94d1f7f722fcb431a5717ef3c59
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2025-08-28 16:13:31 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -5,24 +5,89 @@ const User = require("./models/user");
 
 const app = express()
 app.use(express.json()); //middleware to parse JSON bodies
-app.post("/signup",async(req, res) => {
-  const user= new User(req.body); //create a new user instance with the request body
-  //   {
-  //   //Hard coded data for testing
-  //   // firstName: "sandhya",
-  //   // lastName: "mohanty",
-  //   // emailId: "sandhya@gmail.com",
-  //   // password: "sandhya@12",
-  //   // age: 23,
-  //   // gender: "female"
-  // }
 
+app.post("/signup",async(req, res) => {
   try {
+    const user= new User(req.body);
     const savedUser = await user.save();
     res.status(201).json({message: "User created successfully", user: savedUser});
   } catch (error) {
     res.status(500).json({message: "Error creating user", error: error.message});//error handling
   }
+})
+//get user by emailId
+app.get("/user", async (req, res) => {
+  const  userEmail  = req.body.emailId; //extract emailId from request parameters
+  try {
+    const users = await User.find({ emailId:userEmail }); //find user by emailId
+    if (!users) {
+      return res.status(404).json({ message: "User not found" }); //if user not found, return 404
+    } 
+    res.status(200).json(users); //return user data
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user", error: error.message }); //error handling
+  }
+});
+
+//feed api get/feed -- get vall the users from the database
+app.get("/feed", async(req,res)=>{
+  try {
+    const users = await User.find({}); //fetch all users from the database
+    res.status(200).json(users); //return the list of users
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users", error: error.message }); //errorhandling
+  }
+})
+//user find by id
+app.get("/user/:id",async(req,res)=>{
+  const userId=req.params.id;
+  try {
+    const users=await User.findById({_id:userId})
+    if(!users){
+      return res.status(404).json({message:"user not found"})
+
+    }else{
+      res.status(200).json(users)
+    }
+    
+  } catch (error) {
+    res.status(500).json({message:"error fetching user",error:error.message})
+    
+  }
+})
+//delete user by id
+app.delete("/user/:id",async(req,res)=>{
+  const userId= req.params.id;
+  try{
+    const users = await User.findByIdAndDelete({_id: userId})
+    if(!users){
+      res.status(404).json({message:"user not found"})
+    }else{
+      res.status(200).json({message:"user deleted successfully"})
+    }
+  }catch(error){
+    res.status(500).json({message:"error deleting user",error:error.message})
+  }
+})
+
+//update user by id
+app.patch("/user/:id",async(req,res)=>{
+  const userId= req.params.id;
+  const updateData=req.body;
+  try {
+    const users= await User.findByIdAndUpdate({_id:userId},updateData,{new:true,runValidators:true})
+    if(!users){     
+      res.status(404).json({message:"user not found"})
+    }else{
+      res.status(200).json({message:"user updated successfully",user:users})
+      console.log(users);
+      
+    }
+  } catch (error) {
+    res.status(500).json({message:"error updating user",error:error.message})
+    
+  }
+
 })
 
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,57 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName:{type: String},
-  lastName:{type:String},
-  emailId:{
-    type:String
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["female", "male", "others"].includes(value)) {
+          throw new Error("Gender data not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://nationalchaplains.us/wp-content/uploads/2021/07/dummy-profile-pic.jpg",
+    },
+    about: {
+      type: String,
+      default: "This is some about for this users",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  password:{
-    type:String
-  },
-  age:{
-    type: Number
-  },
-  gender:{
-    type: String
+  {
+    timestamps: true,
   }
-})
+);
+
 //create a model
 const User = mongoose.model("User", userSchema);
 module.exports = User;
