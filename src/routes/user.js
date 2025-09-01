ee9b09a11a2598503815ca6bf062799c12771fc2
sandhya-mohanty <sandhyamohanty1999@gmail.com>
2025-09-01 16:25:51 +0530

    create api for get all accepted  connection for logedin user

--- src/routes/user.js
+++ src/routes/user.js
@@ -15,7 +15,30 @@ userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
     res.status(200).json({message:"data fetched successfully",data:connectionRequest})
     
   } catch (error) {
-    res.status(400).json({})
+    res.status(400).json({message:error.message})
+  }
+})
+//get all accepted  connection for logedin user
+
+userRouter.get("/user/connections",userAuth,async(req,res)=>{
+  try {
+    const logedInUser = req.user
+    const connectionRequest= await ConnectionRequestModel.find({
+      $or:[
+        {fromUserId:logedInUser._id,status:"accepted"},
+        {toUserId:logedInUser._id,status:"accepted"}
+      ]
+    }).populate("fromUserId","firstName lastName photoUrl age skills gender")
+    .populate("toUserId","firstName lastName photoUrl age skills gender")
+    const data = connectionRequest.map((row)=>{
+      if(row.fromUserId._id.toString()===logedInUser._id.toString()){
+        return row.toUserId
+      }
+      return row.fromUserId
+    })
+ res.send(data)
+  } catch (error) {
+    res.status(400).json({message:error.message})
   }
 })
 
